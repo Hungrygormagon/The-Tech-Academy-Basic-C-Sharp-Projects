8b1d3394547de83d048aca6fcedbd0aca137b230
Steven J. Carney <Stevenjcarney@gmail.com>
2020-07-27 11:48:40 -0700

    Math Operators Assignment

---
+++ MathOperators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace MathOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Showing how math operators work in C# programming language");
+            Console.WriteLine("Please enter a number: ");
+            int multNumber = Convert.ToInt32(Console.ReadLine());
+            int multtotal = multNumber * 50;
+            Console.WriteLine(multNumber + " times 50 is equal to: " + multtotal);
+            Console.WriteLine("Please enter a number: ");
+            int addNumber = Convert.ToInt32(Console.ReadLine());
+            int addtotal = addNumber + 25;
+            Console.WriteLine(addNumber + " plus 25 is equal to: " + addtotal);
+            Console.WriteLine("Please enter a number: ");
+            int divNumber = Convert.ToInt32(Console.ReadLine());
+            double divtotal = (double)divNumber/(double)12.5;
+            Console.WriteLine(divNumber + " divided by 12.5 is equal to: " + divtotal);
+            Console.WriteLine("Please enter a number: ");
+            int greaterNumber = Convert.ToInt32(Console.ReadLine());
+            bool greatertotal =greaterNumber > 50;
+            Console.WriteLine("Is " + greaterNumber + " greater than 50? " + greatertotal);
+            Console.WriteLine("Please enter a number: ");
+            int remainderNumber = Convert.ToInt32(Console.ReadLine());
+            int remaindertotal = remainderNumber % 7;
+            Console.WriteLine("The remainder of " + remainderNumber + " and 7 is " + remaindertotal);
+        }
+    }
+}
