a59229b857822a9589632ece3804f70a53e91925
Steven J. Carney <Stevenjcarney@gmail.com>
2020-07-27 16:52:37 -0700

    upload Package Express App

---
+++ PackageExpressAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PackageExpressAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thank you for choosing Package Express!");
+            Console.WriteLine("Please enter your package weight: ");
+            int pkgWeight = Convert.ToInt32(Console.ReadLine());
+            if (pkgWeight > 50)
+            {
+                Console.WriteLine("We're sorry, that package is unable to be shipped through us. Have a nice day.");
+            };
+            Console.WriteLine("Please enter your package height: ");
+            int pkgHeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your package length: ");
+            int pkgLength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your package width: ");
+            int pkgWidth = Convert.ToInt32(Console.ReadLine());
+            int pkgTotalDim = pkgHeight + pkgWidth + pkgLength;
+            int pkgDimensions = pkgHeight * pkgLength * pkgWeight;
+            int pkgCost = pkgDimensions / 100;
+            string pkgDecision = (pkgTotalDim > 50) ? "We're sorry, that package is unable to be shipped through us. Have a nice day." : "The cost for shipping this package will be: $" + pkgCost + ".00.";
+            Console.WriteLine(pkgDecision);
+            Console.WriteLine("Thank you for choosing Package Express, press any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
