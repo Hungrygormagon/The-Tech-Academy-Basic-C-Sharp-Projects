94642a4e3971e8b25dfcc0c2c919360cdd4a0852
Steven J. Carney <Stevenjcarney@gmail.com>
2020-07-27 18:18:48 -0700

    Add String building assignment

---
+++ StringBuilding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringOne = "Hello!";
+            string stringTwo = " I am a program ";
+            string stringThree = "written in C# language";
+
+            Console.WriteLine(stringOne + stringTwo + stringThree);
+
+            string stringFour=stringOne.ToUpper();
+            Console.WriteLine(stringFour);
+
+            StringBuilder sb = new StringBuilder("\t Hello world!");
+            sb.Append("This is an example of the stringbuilder utility.");
+            sb.AppendLine("It is useful for adding information to new strings,");
+            sb.AppendLine("and it helps to contain memory, and not overwhelm a computer");
+
+            Console.WriteLine(sb.ToString());
+
+
+        }
+    }
+}
