caf0d7bfb45bea561ddd3c59b3860fea09d92235
Steven J. Carney <Stevenjcarney@gmail.com>
2020-08-08 12:00:01 -0700

    Changes made to IF-ELse nesting

--- PackageExpressAssignment/Program.cs
+++ PackageExpressAssignment/Program.cs
@@ -9,23 +9,38 @@ namespace PackageExpressAssignment
             Console.WriteLine("Thank you for choosing Package Express!");
             Console.WriteLine("Please enter your package weight: ");
             int pkgWeight = Convert.ToInt32(Console.ReadLine());
-            if (pkgWeight > 50)
+            if (pkgWeight >= 50)
             {
-                Console.WriteLine("We're sorry, that package is unable to be shipped through us. Have a nice day.");
+                Console.WriteLine("We're sorry, that package is unable to be shipped through us. Have a nice day. Press any key to exit");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter your package height: ");
+                int pkgHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter your package length: ");
+                int pkgLength = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter your package width: ");
+                int pkgWidth = Convert.ToInt32(Console.ReadLine());
+                int pkgTotalDim = pkgHeight + pkgWidth + pkgLength;
+                int pkgDimensions = pkgHeight * pkgLength * pkgWeight;
+                int pkgCost = pkgDimensions / 100;
+                if (pkgTotalDim >= 50)
+                {
+                    Console.WriteLine("We're sorry, that package is unable to be shipped through us. Have a nice day. Press any key to exit");
+                    Console.ReadLine();
+                }
+                else {
+                    Console.WriteLine("The package is shippable! It will cost $" + pkgCost + ".00 to ship.");
+                    Console.WriteLine("Thank you for choosing Package Express, press any key to continue.");
+                    Console.ReadLine();
+                }
+                
             };
-            Console.WriteLine("Please enter your package height: ");
-            int pkgHeight = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter your package length: ");
-            int pkgLength = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter your package width: ");
-            int pkgWidth = Convert.ToInt32(Console.ReadLine());
-            int pkgTotalDim = pkgHeight + pkgWidth + pkgLength;
-            int pkgDimensions = pkgHeight * pkgLength * pkgWeight;
-            int pkgCost = pkgDimensions / 100;
-            string pkgDecision = (pkgTotalDim > 50) ? "We're sorry, that package is unable to be shipped through us. Have a nice day." : "The cost for shipping this package will be: $" + pkgCost + ".00.";
-            Console.WriteLine(pkgDecision);
-            Console.WriteLine("Thank you for choosing Package Express, press any key to continue.");
-            Console.ReadLine();
+
         }
     }
 }
+
+
+
