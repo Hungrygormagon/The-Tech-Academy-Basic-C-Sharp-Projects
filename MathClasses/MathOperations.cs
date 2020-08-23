9a1c1aa94a19c121a6b9fbe4f05557ec0c411917
Steven J. Carney <Stevenjcarney@gmail.com>
2020-08-23 10:12:25 -0700

    Add files via upload

---
+++ MathClasses/MathOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathClasses
+{
+    public class MathOperations
+    {
+        public int DivisionMethod(int Num1) 
+        {
+            int result = Num1 / 3;
+            Console.WriteLine(Num1 + " divided by 3 is equal to " + result);
+            return result;
+        }
+        public int MultiplicationMethod(int Num1)
+        {
+            int result = Num1 * 50;
+            Console.WriteLine(Num1 + " multiplied by 50 is equal to " + result);
+            return result;
+        }
+        public int AdditionMethod(int Num1)
+        {
+            int result = Num1 + 12358;
+            Console.WriteLine(Num1 + " added to 12358 is equal to " + result);
+            return result;
+        }
+    }
+}
---
+++ MathClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MathClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Math Class!");
+            Console.WriteLine("Please choose a non-zero number: ");
+            int Num1 = Convert.ToInt32(Console.ReadLine());
+            MathOperations operations = new MathOperations();
+            operations.DivisionMethod(Num1);
+            operations.MultiplicationMethod(Num1);
+            operations.AdditionMethod(Num1);
+
+
+            Console.ReadLine();
+        }
+    }
+}
