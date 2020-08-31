415c6fa0c42a83f50012d9f736076eaba69ab8eb
Steven J. Carney <Stevenjcarney@gmail.com>
2020-08-31 14:37:35 -0700

    Upload filewriting assignment

---
+++ FileWriting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace FileWriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a number!");
+            string userNumber = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\sjcso\usenumber.txt", true))
+            {
+                file.WriteLine(userNumber);
+            }
+            Console.WriteLine(File.ReadAllText(@"C:\Users\sjcso\usenumber.txt"));
+        }
+    }
+}
